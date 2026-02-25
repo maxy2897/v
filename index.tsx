da12a764c338a375e17d2dac97cb594b884d5b86
Bodipo <admin@bodipobusiness.es>
2026-02-25 02:48:30 +0100

    Add country flag emoji polyfill for Windows

--- index.tsx
+++ index.tsx
@@ -3,7 +3,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { polyfillCountryFlagEmojis } from "country-flag-emoji-polyfill";
 
+// Fuerza a Windows y navegadores web a renderizar banderas en lugar de letras
+polyfillCountryFlagEmojis();
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
