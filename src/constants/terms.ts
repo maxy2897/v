47b248befa18400880a4b2e7b64efe80959548a5
Bodipo <admin@bodipobusiness.es>
2026-04-22 19:50:40 +0200

    Remove manual numbering from Terms and Conditions titles

--- src/constants/terms.ts
+++ src/constants/terms.ts
@@ -1,46 +1,46 @@
 export const TERMS_AND_CONDITIONS = [
     {
-        title: "1. Condiciones generales",
+        title: "Condiciones generales",
         content: "Las presentes Condiciones Generales son de aplicación en el momento en que BODIPO BUSINESS (de ahora en adelante BODIPO) acepta el envío, salvo pactos o condiciones especificadas en otro contrato de prestación de servicios de logística y transporte. Las Condiciones Generales aquí presentadas también serán de aplicación a las cartas de portes emitidas electrónicamente."
     },
     {
-        title: "2. Datos del envío y restricciones del servicio",
-        content: "El remitente o cargador proporciona bajo su responsabilidad los datos contractuales relativos a la naturaleza de la mercancía, remitente y destinatario. Estos datos no serán comprobados por BODIPO salvo grave riesgo inminente u orden de la autoridad. BODIPO no ofrece servicios de transporte de mercancías peligrosas (ADR) o perecederas, por lo que el envío de este tipo de mercancía por el remitente exonera a BODIPO de cualquier responsabilidad contractual o extracontractual, en cuyo caso BODIPO se reserva el derecho a suspender el transporte. En caso de que los datos facilitados por el remitente no sean veraces (peso, dimensiones de la mercancía), BODIPO, una vez auditado dicha mercancía, podrá cobrar sobrecostes al cliente/usuario. El cliente está obligado a enviar o hacer fotos de los bultos a enviar, y donde quede constancia de las dimensiones de dicho bulto y peso."
+        title: "Datos del envío y restricciones del servicio",
+        content: "El remitente or cargador proporciona bajo su responsabilidad los datos contractuales relativos a la naturaleza de la mercancía, remitente y destinatario. Estos datos no serán comprobados por BODIPO salvo grave riesgo inminente u orden de la autoridad. BODIPO no ofrece servicios de transporte de mercancías peligrosas (ADR) o perecederas, por lo que el envío de este tipo de mercancía por el remitente exonera a BODIPO de cualquier responsabilidad contractual o extracontractual, en cuyo caso BODIPO se reserva el derecho a suspender el transporte. En caso de que los datos facilitados por el remitente no sean veraces (peso, dimensiones de la mercancía), BODIPO, una vez auditado dicha mercancía, podrá cobrar sobrecostes al cliente/usuario. El cliente está obligado a enviar o hacer fotos de los bultos a enviar, y donde quede constancia de las dimensiones de dicho bulto y peso."
     },
     {
-        title: "3. Plazos de entrega",
+        title: "Plazos de entrega",
         content: "Los envíos deben ser retirados en los centros logísticos de BODIPO: Madrid, Malabo, Bata, Yaounde. No obstante, si lo desea el cliente, se puede realizar la entrega de los bultos en la dirección que éste señale siempre y cuando pague por dicho servicio extra. Los plazos aproximados de entrega son los siguientes: De Madrid — Malabo (2 días), Madrid — Bata (3 días), De Bata — Malabo y viceversa (1 día). Los servicios se prestan de lunes a domingo, excluyéndose del cómputo de plazos los días festivos en la localidad de origen y de destino. Los plazos de entrega pueden verse afectados por causas organizativas de BODIPO según el destino. El cliente está obligado a retirar la mercancía en el centro logístico de la dirección de destino, una vez que se le avise de que dicha mercancía ya se encuentra en el centro logístico en el plazo máximo de 4 días, a sabiendas de que a partir del quinto se le cobrará 2000 fcos Cfa/ día de retraso."
     },
     {
-        title: "4. Retraso de la entrega",
+        title: "Retraso de la entrega",
         content: "No se considerará retraso en la entrega aquel producido por ausencia o cambio de domicilio del destinatario, impago de portes, reembolsos, cierre por vacaciones, entregas fuera de plazo en segundo intento de entrega o posteriores, causas de fuerza mayor o caso fortuito, o cualesquiera otras no imputables a BODIPO."
     },
     {
-        title: "5. Responsabilidad y seguro",
+        title: "Responsabilidad y seguro",
         content: "En defecto de declaración expresa del valor de la mercancía antes de la contratación del servicio de transporte, el límite de responsabilidad de BODIPO por daño o falta de la mercancía consignada para el transporte, tomando como base el valor que tuvieran en el momento y lugar que BODIPO las recibió para su transporte, siendo el límite máximo a indemnizar, el resultado que se obtenga al calcular 1/3 del IPREM diario (6,67€/kg). El cliente podrá contratar un seguro que cubra el valor de reposición de la mercancía o los daños que estas puedan sufrir hasta el total del valor declarado con carácter previo a la realización del transporte. El coste del seguro es de 30€."
     },
     {
-        title: "6. Reclamación y reservas",
+        title: "Reclamación y reservas",
         content: "En el caso de daños o pérdidas en la mercancía, el destinatario deberá indicar en el momento de la entrega y por escrito en el albarán en qué consisten dichos daños o faltas en un plazo de 24 horas si son daños aparentes. En caso contrario se presume que la mercancía ha sido entregada correctamente. Si los daños o pérdidas fuesen imposibles de apreciar en el momento de la entrega deberá realizarse una reclamación por escrito a BODIPO en un plazo no superior a 7 días naturales y acreditar que dichos daños o pérdidas son imputables a BODIPO. Con el objeto de proceder a la tramitación de las reclamaciones por daños o roturas en cualquier modalidad de contratación, será imprescindible la conservación de los embalajes de la mercancía siniestrada por parte del cliente y su exhibición al personal de BODIPO."
     },
     {
-        title: "7. Embalaje",
+        title: "Embalaje",
         content: "Los envíos se entregarán con el embalaje adecuado y suficiente. En general, se aceptarán envíos que usen el embalaje del fabricante original, o el del remitente, salvo que se aprecie una clara y manifiesta falta de protección, de acuerdo a su contenido, en cuyo caso BODIPO se reserva el derecho de aceptarlos para su envío por insuficiencia de embalaje. En caso de un embalaje insuficiente, BODIPO la realizará correctamente, repercutiendo al cliente los gastos por dicho servicio. En caso de rechazo de bultos por insuficiencia de embalaje u otras reservas suficientemente motivadas en la carta de porte y aceptación por parte del remitente de las mismas, BODIPO no responderá de las averías que pudiesen producirse en las mercancías, bastando a tal efecto la marca del cliente en el espacio correspondiente del albarán. BODIPO se encargará de la carga, descarga, estiba y desestiba de la mercancía."
     },
     {
-        title: "8. Reembolsos",
+        title: "Reembolsos",
         content: "El servicio de reembolso sólo está disponible para clientes abonados o con código, cuyas estipulaciones están recogidas en el contrato. Para más información, póngase en contacto con nuestro servicio de atención al cliente en el teléfono +34 641 992 110 o info@bodipobusiness.com."
     },
     {
-        title: "9. Depósito y enajenación de la mercancía",
+        title: "Depósito y enajenación de la mercancía",
         content: "En los casos de impedimentos al transporte o a la entrega, impago de portes o de gastos o del reembolso, BODIPO podrá descargar la mercancía y custodiarla por cuenta de quien tenga derecho sobre las mismas. Si transcurridas dos semanas no hubiese recibido nuevas instrucciones podrá proceder a enajenarlas para hacer frente al precio del transporte o/y almacenaje de la mercancía además de los gastos que hubiese generado hasta el momento. BODIPO además podrá abrir el paquete y en caso de que el valor venal de la mercancía fuera ínfimo, podrá destruirla y reclamar al cliente la totalidad de la cantidad debida. BODIPO no tendrá responsabilidad frente a terceros que manifiesten tener algún derecho sobre la mercancía."
     },
     {
-        title: "10. Despacho de aduanas",
+        title: "Despacho de aduanas",
         content: "El remitente deberá proporcionar la documentación adecuada para el Despacho de Aduana, siendo responsable de su veracidad y validez. Los gastos del despacho aduanero de exportación serán por cuenta del cliente. Salvo pacto en contrario, los gastos de despacho aduanero de importación, así como otros impuestos y gastos repercutibles y sanciones aduaneras serán por cuenta del consignatario (destinatario) y deberán satisfacerse en el momento de entrega de la mercancía. Las tarifas de este apartado no incluyen los impuestos (Ej. IVA, IGIC) u otros gastos repercutibles que sean necesarios para poder llevar a efecto la entrega del envío en destino y que conforme a lo establecido en el punto anterior serán satisfechos por el consignatario."
     },
     {
-        title: "12. Jurisdicción",
+        title: "Jurisdicción",
         content: "Cualquier diferencia sobre la aplicación o interpretación de las presentes condiciones generales de transporte y con independencia a la cuantía de la controversia, así como cualquier contrato establecido en virtud de los mismos, se someterá a la competencia de la Junta Arbitral de Transporte que las partes elijan de manera voluntaria."
     }
 ];
